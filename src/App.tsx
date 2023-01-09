45f626dbcbdca29d1020900d293993bbfb9828f9
Pedro Henrique Dionisio <pedrohenriquedionisioalves@gmail.com>
2023-01-08 23:14:55 -0300

    global styles and default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './styles/GlobalStyles';
+import { defaultTheme } from './styles/themes/default';
+
 function App() {
-  return <h1>Waiterapp</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h1>Waiter App</h1>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  colors: {
+    primary: {
+      100: '#FFABAD',
+      500: '#D73035',
+      900: '#8A1114',
+    },
+    gray: {
+      100: '#CCCCCC',
+      300: '#999999',
+      500: '#666666',
+      900: '#333333',
+    },
+    white: '#FFFFFF',
+    background: '#F2F2F2',
+  },
+};
